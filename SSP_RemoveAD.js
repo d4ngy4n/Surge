0f306117714796a3d0b5742c2ee23798396ae5aa
Mojit0 <Mojit000@users.noreply.github.com>
2023-08-05 08:43:06 +0800

    SSP_RemoveAD.js

--- SSP_RemoveAD.js
+++ SSP_RemoveAD.js
@@ -8,7 +8,7 @@ if (obj.data) {
   // 少数派-开屏广告
   if (url.includes("ios.sspai.com/api/v3/recommend/page/get?ad_morning_paper_show=0&page=1&page_size=20&type=ios_home_modal")) {
     obj.data.forEach((element) => {
-      element.is_show = false;
+      element.ios_show = false;
       element.show_time = 0;
     });
   } 
