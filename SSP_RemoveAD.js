1c1419fb661cdea9c3908a036250ad8558cf5513
Mojit0 <Mojit000@users.noreply.github.com>
2023-08-05 08:22:58 +0800

    Create

--- SSP_RemoveAD.js
+++ SSP_RemoveAD.js
@@ -5,7 +5,7 @@ if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
 if (obj.data) {
-  // 什么值得买-开屏广告
+  // 少数派-开屏广告
   if (url.includes("ios.sspai.com/api/v3/recommend/page/get?ad_morning_paper_show=0&page=1&page_size=20&type=ios_home_modal")) {
     obj.data.forEach((element) => {
       element.is_show = false;
