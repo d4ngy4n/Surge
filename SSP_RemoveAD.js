9f1734174f43ae7a982f89084690d0567a0b675e
Mojit0 <Mojit000@users.noreply.github.com>
2023-08-05 08:22:25 +0800

    Create

---
+++ SSP_RemoveAD.js
@@ -0,0 +1,16 @@
+// 2023-08-06 08:22
+
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+if (obj.data) {
+  // 什么值得买-开屏广告
+  if (url.includes("ios.sspai.com/api/v3/recommend/page/get?ad_morning_paper_show=0&page=1&page_size=20&type=ios_home_modal")) {
+    obj.data.forEach((element) => {
+      element.is_show = false;
+    });
+  } 
+}
+
+$done({ body: JSON.stringify(obj) });
