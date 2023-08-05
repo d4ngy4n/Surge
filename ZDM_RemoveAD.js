cf74c455e6346db1b192521a6e2a0f1501495596
Mojit0 <Mojit000@users.noreply.github.com>
2023-08-05 09:01:48 +0800

    Create

---
+++ ZDM_RemoveAD.js
@@ -0,0 +1,16 @@
+// 2023-08-06 08:22
+
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+if (obj.data) {
+  // 什么值得买-开屏广告
+  if (url.includes("s3.zdmimg.com/cpm/api/v1/advert_distribution/get_all_advertise")) {
+    obj.data.forEach((element) => {
+      element.is_show_ad = 0;
+    });
+  } 
+}
+
+$done({ body: JSON.stringify(obj) });
