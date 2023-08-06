f4699510192dc5d57e664c41bedc41904a74c956
Mojit0 <yan199219dang@gmail.com>
2023-08-06 14:14:06 +0800

    Update SSP_Homepage_AD.js

--- SSP_Homepage_AD.js
+++ SSP_Homepage_AD.js
@@ -1,11 +1,10 @@
 // 2023-08-06 08:22
-
 const url = $request.url;
 if (obj.data) {
   // 少数派-开屏广告
   if (url.includes("ios.sspai.com/api/v3/combo/recommend")) {
     obj.data.forEach((element) => {
-      element.title = "Mojit0 Hello";
+      element.title = "Mojit0";
     });
   } 
 }
