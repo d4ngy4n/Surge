14f8130767d12deaeeb0d7ca2f8971a6518103f5
Mojit0 <yan199219dang@gmail.com>
2023-08-06 14:04:42 +0800

    Update SSP_Homepage_AD.js

--- SSP_Homepage_AD.js
+++ SSP_Homepage_AD.js
@@ -1,18 +1,12 @@
 // 2023-08-06 08:22
 
 const url = $request.url;
-if (!$response.body) $done({});
-let obj = JSON.parse($response.body);
-let data = []
 if (obj.data) {
   // 少数派-开屏广告
-  if(url.include("ios.sspai.com/api/v3/combo/recommend/page/get?page=1&page_size=50")) {
-   obj.data.forEach((element) => {
-      if(element.nickname != "少数派商店") {
-        data.push(element)
-      }
-    })
-    obj.data = data
-  }
+  if (url.includes("ios.sspai.com/api/v3/combo/recommend/page/get?page=1&page_size=50")) {
+    obj.data.forEach((element) => {
+      element.nickname = "Mojit0";
+    });
+  } 
 }
 $done({ body: JSON.stringify(obj) });
