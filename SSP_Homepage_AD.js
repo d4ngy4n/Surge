eb79e2a6f24e765410b033758d5f75efc6f82703
Mojit0 <yan199219dang@gmail.com>
2023-08-06 12:51:17 +0800

    Update SSP_Homepage_AD.js

--- SSP_Homepage_AD.js
+++ SSP_Homepage_AD.js
@@ -8,7 +8,7 @@ if (obj.data) {
   // 少数派-开屏广告
   if(url.include("ios.sspai.com/api/v3/combo/recommend/page/get?page=1&page_size=50")) {
     obj.data = obj.data.filter((element) => {
-      return element.corner.memo != "广告"
+      element.corner.memo != "广告"
     })
   }
 }
